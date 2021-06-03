e4de48bb007aba70845f521ff07e054b025c855b
Will Leyton <wleylop22@gmail.com>
2021-06-03 10:49:55 -0500

    add run

---
+++ lib/beginning_elixir.ex
@@ -0,0 +1,11 @@
+defmodule BeginningElixir do
+  @doc """
+  Hello
+  ## Examples
+    iex> BeginningElixir.hello("name")
+    iex> "Hello World name"
+  """
+  def hello(name) do
+    "Hello World #{name}"
+  end
+end
