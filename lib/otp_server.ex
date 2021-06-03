7797f1e2f640413a9593085d6995d95358726511
Will Leyton <wleylop22@gmail.com>
2021-06-03 10:49:32 -0500

    Add otp server

---
+++ lib/otp_server.ex
@@ -0,0 +1,49 @@
+defmodule Otp.Actor do
+  use GenServer
+
+  @impl true
+  def init(initial_number) do
+    {:ok, initial_number}
+  end
+
+  # :next_number: Client request
+  # _from : Client PID
+  # current_state: Current State
+  @impl true
+  def handle_call(:next_number, _from, current_state) do
+    next_state = current_state + 1
+    response = next_state
+    # action:
+    #  :reply -> reply to client
+    # {action, client response, new state}
+    {:reply, response, next_state}
+  end
+
+  @impl true
+  def handle_cast({:update_number, number}, _current_state) do
+    {:noreply, number}
+  end
+end
+
+defmodule Otp.Client do
+  @actor Otp.Actor
+
+  def run do
+    # case GenServer.start_link(Otp.Sequence.Server, 100) do
+    case GenServer.start_link(Otp.Actor, 100, name: @actor) do
+      {:ok, _pid} ->
+        GenServer.call(@actor, :next_number)
+        GenServer.call(@actor, :next_number)
+        GenServer.call(@actor, :next_number)
+        response = GenServer.call(@actor, :next_number)
+        IO.puts("[Response: #{response}]")
+        GenServer.cast(@actor, {:update_number, 22})
+        response = GenServer.call(@actor, :next_number)
+        IO.puts("[Response: #{response}]")
+        :sys.get_status(@actor)
+
+      {:error, _reason} ->
+        IO.puts("Error")
+    end
+  end
+end
