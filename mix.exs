bd0077a8e698dbf67ab6651667eea0b46b89ad14
Will Leyton <wleylop22@gmail.com>
2021-06-16 10:24:46 -0500

    Update

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule BeginningElixir.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :observer]
     ]
   end
 
@@ -23,7 +23,8 @@ defmodule BeginningElixir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:ex_doc, "~> 0.24", only: [:dev], runtime: false}
+      {:ex_doc, "~> 0.24", only: [:dev], runtime: false},
+      {:uuid, "~> 1.1"}
     ]
   end
 end
